f2bce3fadacb744ec843a61601eae553858a6095
cihanb <johndoe@example.com>
2018-01-30 16:24:52 -0800

    added travis tests.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# ```redis-py``` Docker Image for Redis & Redis Enterprise
-Sample Docker image based on Python 3 with redis-py for testing simple python based Redis apps.
+## ```redis-py``` 
+# Docker Image for Testing Python Based Apps with Redis & Redis Enterprise
+Sample docker image based on ```python 3``` with ```redis-py``` for testing simple python based Redis apps.
 
-Simply run ```Redis-Python-Sample.py``` with hostname and port for a Redis database to test Redis's SET and GET operations:
+Simply run ```Redis-Python-Sample.py``` with ```hostname``` and ```port``` for the Redis database to test SET & GET commands:
 ```
 # python Redis-Python-Sample.py 172.17.0.3 12000
 
@@ -10,5 +11,6 @@ Set key 'key1' to value '123' on host=172.17.0.3 and port=12000
 Get key 'key1' and validate value is '123' on host=172.17.0.3 and port=12000
 DB TEST PASSED
 ```
-
-For detailed instructions on how to run Redis Enterprise to test out python with Redis visit [Redis Enterprise Hub](https://hub.docker.com/r/redislabs/redis/) or [Redis Hub](https://hub.docker.com/_/redis/).
+# 
+# Step by Step Guides
+For detailed instructions on how to test python based Redis apps with Redis or Redis Enterprise visit [Redis Enterprise Hub](https://hub.docker.com/r/redislabs/redis/) or [Redis Hub](https://hub.docker.com/_/redis/).
---
+++ tests/simple_test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# The MIT License (MIT)
+#
+# Copyright (c) 2018 Redis Labs
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Script Name: settings.sh
+# Author: Cihan Biyikoglu - github:(cihanb)
+
+#read settings
+oss_db_port=6379
+oss_host_name="localhost"
+
+test_db(){
+    echo "test_db()"
+    docker exec -it redis-python "python ../Redis-Python-Sample.py $oss_host_name $oss_db_port"
+}
+
+### START HERE ###
+#launch the container
+echo "docker run -d --name redis-python redislabs/redis-py"
+eval "docker run -d --name redis-python redislabs/redis-py"
+
+#call test_db
+test_db()
+
